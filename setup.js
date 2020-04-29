06326ec2792f4bc31bf8eee2571c1227a0007aa2
Real-SeRyKi <45773844+Real-SeRyKi@users.noreply.github.com>
2020-04-29 21:54:39 +0200

    Small improvement
    Small improvement to setup.js

--- setup.js
+++ setup.js
@@ -2,8 +2,9 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const settings = require('./settings.json')
 client.on('ready', () => {
-
-    client.channels.cache.get(settings.servers[0].ids.channelid).send("Server");
+    for (i = 0; i < settings.servers.length; i++) {
+        client.channels.cache.get(settings.servers[i].ids.channelid).send("Server status");
+    }
 })
 
 client.login(settings.token)
